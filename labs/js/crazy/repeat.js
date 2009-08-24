a5900cc5283fdda7f5d2ac89f141fac5619c8dd4
闲耘™ <hotoo@users.noreply.github.com>
2009-08-24 07:22:19 +0000

    more repeat functions

--- labs/js/crazy/repeat.js
+++ labs/js/crazy/repeat.js
@@ -1,24 +1,72 @@
-
-function repeat(s,i){
+/**
+ * 把字符串s重复i次。
+ * @param {String} s 被重复的字符串。
+ * @param {Number} i 重复的次数。
+ * @return {String} 返回重复后得到的新字符串。
+ */
+function repeat_new_arr_join(s,i){
 	return new Array(1+(i||0)).join(s);
 }
-function repeat2(s,i){
+function repeat_for_minus_arr_join(s,i){
 	var t=[];
     for(; i>0; i--){t[t.length]=s;}
     return t.join("");
 }
-function repeat3(s,i){
+function repeat_for_minus_str_append(s,i){
 	var t="";
     for(; i>0; i--){t+=s;}
     return t;
 }
+function repeat_for_plus_arr_join(s,i){
+    var t=[];
+    for(var k=0; k<i; k++){ t[k]=s;}
+    return t.join("");
+}
+function repeat_for_plus_str_append(s,i){
+    var t="";
+    for(var k=0; k<i; k++){t+=s;}
+    return s;
+}
+function repeat_while_minus_arr_join(s,i){
+    var t=[];
+    while(i-- > 0){t[t.length]=s;}
+    return t.join("");
+}
+function repeat_while_minus_str_append(s,i){
+    var t="";
+    while(i-- > 0){t+=s;}
+    return t;
+}
+function repeat_while_plus_arr_join(s,i){
+    var t=[], k=0;
+    while(k++ < i){t[k]=s;}
+    return t.join("");
+}
+function repeat_while_plus_str_append(s,i){
+    var t="", k=0;
+    while(k++ < i){t+=s;}
+    return t;
+}
+function repeat_recursion_callee(s,i){
+    var r=arguments[2]||"";
+    return i?arguments.callee(s,i-1,r+s):r;
+}
+function repeat_recursion_name(s,i){
+    var r=arguments[2]||"";
+    return i?repeat_recursion_name(s,i-1,r+s):r;
+}
+
+
 
 
 function crazyIt(){
     var c=new Crazy();
-    c.funs=[repeat, repeat2, repeat3];
-    c.args=["0", 3];
-    c.timer=100000;
+    c.funs=[repeat_new_arr_join, repeat_for_minus_arr_join, repeat_for_minus_str_append,
+        repeat_for_plus_arr_join, repeat_for_plus_str_append, repeat_while_minus_arr_join,
+        repeat_while_minus_str_append, repeat_while_plus_arr_join, repeat_while_plus_str_append,
+        repeat_recursion_callee, repeat_recursion_name];
+    c.args=["0", 30];
+    c.timer=10000;
     c.test();
 }
 
