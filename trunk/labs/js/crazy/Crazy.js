f4afde06ea5b0b8cbba7994cc98a2ddd8ebe6bb0
hotoo.cn <hotoo.cn@57cb46fc-d67f-11dd-86c2-0f8696b7b6f9>
2009-08-24 13:42:22 +0000

    version 1.2, outline css, multi crazy test.

--- trunk/labs/js/crazy/Crazy.js
+++ trunk/labs/js/crazy/Crazy.js
@@ -1,18 +1,24 @@
 /**
- * @param 
+ * @overview {class} Crazy - Performance Test Framework.
+ * @param {Array<Function>} function list.
+ * @param {Array} arguments for execute function item.
+ * @param {Number} times loop execute function item times.
+ * @param {Function} callback execute when all performance test complete.
  * @author 闲耘(mail[AT]xianyun.org)
+ * @version 1.0 2009/08/23
  */
-var Crazy=function(funs,args,time){
+var Crazy=function(funs, args, times, callback){
     this.funs=funs;
+    this._func=null; //function copy.
     this.args=args;
-    this.times=time||10000;
+    this.times=times||10000;
     this._result=[];
+    this.callback=callback;
 };
 Crazy.prototype.test=function(){
 	if(!this.funs || this.funs.length==0){return;}
-	//for (var i=0,l=Crazy.funs.length; i<l; i++){
-        this._test1(this.funs.shift());
-	//}
+    this._func = this.funs.concat();
+    this._test1(this._func.shift());
 };
 Crazy.prototype._test1=function(f){
     var d = new Date();
@@ -20,32 +26,33 @@ Crazy.prototype._test1=function(f){
         f.apply(f, this.args);
     }
     d = new Date()-d;
-    var n=funame(f),ME=this;
+    var n=funame(f), ME=this;
     this._result[this._result.length]=[n,d];
     window.setTimeout(function(){
-        if(ME.funs.length){
-            ME._test1(ME.funs.shift());
+        if(ME._func.length){
+            ME._test1(ME._func.shift());
         }else{
             ME.print();
+            if(ME.callback){ME.callback();}
         }
     }, 100);
 };
 Crazy.prototype.print=function(){
-    var s="", max=this._result[0][1];
-    for (var i=0,l=this._result.length; i<l; i++){
-        if(this._result[i][1]>max){
-            max=this._result[i][1];
-        }
+    var s="", max=this._result[0][1], min=this._result[0][1], FAST=85;
+    for (var i=1,l=this._result.length; i<l; i++){
+        if(this._result[i][1]>max){max=this._result[i][1];}
+        if(this._result[i][1]<min){min=this._result[i][1];}
     }
-    s=['<table class="result">','<thead><tr>',
-        '<th width="200">Function Name</th>','<th width="100">Timer</th>',
-        '<th width="200">Rate</th>','</tr></thead>','<tbody>'];
+    s=['<table class="result">','<caption><code>',this._result[0][0],'(<i>',this.args.join("</i>, <i>"),'</i>); ...</code></caption>','<thead><tr>',
+        '<th width="260">Function Name</th>',
+        '<th width="200">Time(Rate)</th>','<th width="200">Performance</th>','</tr></thead>','<tbody>'];
     for (i=0,l=this._result.length; i<l; i++){
-        var r=(this._result[i][1]/max*100).toFixed(2);
+        var r=(100*this._result[i][1]/max).toFixed(2),
+            p=(100*min/this._result[i][1]).toFixed(2);
         s.push('<tr>',
-            '<td><strong>',this._result[i][0],'</strong></td>',
-            '<td>',this._result[i][1],'</td>',
-            '<td><div class="rate"><div style="width:',r,'%;"></div><cite>',r,'</cite></div></td>',
+            '<td>',(p>=FAST?'<span style="color:#FF7D29;">':""),this._result[i][0],(p>=FAST?"<span>":""),'</td>',
+            '<td><div class="rate"><div style="width:',r,'%;"></div><cite>',this._result[i][1],'ms (',r,'%)</cite></div></td>',
+            '<td><div class="perf',(p==100?" perf-best":(p>=FAST?" perf-fast":"")),'"><div style="width:',p,'%;"></div><cite>',p,'%</cite></div></td>',
             '</tr>');
     }
     s.push('</tbody>','</table>');
