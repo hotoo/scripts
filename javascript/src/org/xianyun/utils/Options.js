2f738056c1bc5648ee0ec563d886dd7dd1a6fdc2
闲耘™ <hotoo@users.noreply.github.com>
2009-09-22 16:20:54 +0000

    <select><option>...

---
+++ javascript/src/org/xianyun/utils/Options.js
@@ -0,0 +1,52 @@
+/*<![CDATA[*/
+/**
+ * &lt;select&gt; HTMLElement operations.
+ * @param {HTMLElement} sel &lt;select&gt; element object.
+ * @author 闲耘 (mail[AT]xianyun.org)
+ * @version 1.0, 2009/09/22
+ */
+var Options = function(sel){
+    this.sel = sel;
+};
+/**
+ * create new option item and select it, if exist same one, select it.
+ * @param {String} txt option element's text for display.
+ * @param {String} val value of option for form submit.
+ * @return {Boolean} 
+ */
+Options.prototype.create = function(txt, val){
+    var ops=this.sel.options, l=ops.length;
+    for(var i=0; i<l; i++){
+        if(ops[i].value == (val||txt) || ops[i].text==txt){
+            ops[i].selected = true;
+            return true;
+        }
+    }
+    ops[l] = new Option(txt, val||txt);
+    ops[l].selected = true;
+    return true;
+};
+/**
+ * change selected option's text and value.
+ * @param {String} txt option name/text for display.
+ * @param {String} val option value.
+ * @return {Boolean}
+ */
+Options.prototype.change = function(txt, val){
+    var ops = this.sel.options;
+    if(!ops.length){return false;}
+    ops[sel.selectedIndex].value = val||txt;
+    ops[sel.selectedIndex].text = txt;
+    return true;
+};
+/**
+ * remove selected option item.
+ * @return {Boolean}
+ */
+Options.prototype.remove = function(){
+    var ops = this.sel.options;
+    if(!ops.length){return false;}
+    sel.remove(sel.selectedIndex);
+    return true;
+};
+/*]]>*/
